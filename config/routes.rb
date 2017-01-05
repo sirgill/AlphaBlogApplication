e57cba1bb9696bd2b56bae5f982f278b9ee4dcb5
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2017-01-04 20:37:41 -0500

    Make app production ready with Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  get 'pages/home', to:'pages#home'
+  root 'pages#home'
 
-  get 'pages/about', to:'pages#about'
+  get 'about', to:'pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
