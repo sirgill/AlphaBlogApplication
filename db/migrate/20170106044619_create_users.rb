1927ccc49a27d2cbbb4ce5a75a22c0e1bc5eb6e7
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2017-01-06 10:36:55 -0500

    add association between user and articles and fixed migration error

--- db/migrate/20170106044619_create_users.rb
+++ db/migrate/20170106044619_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.timestampe
+      t.timestamp
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105030501) do
+ActiveRecord::Schema.define(version: 20170106151811) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
   end
 
 end
