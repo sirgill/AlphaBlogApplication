944e724d1dd7afaf874c750a5446c7c5c396a0be
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2017-02-14 17:11:42 -0500

    Updated title length in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   belongs_to :user
   has_many :article_categories
   has_many :categories, through: :article_categories
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 600 }
   validates :description, presence: true, length: { minimum: 10, maximum: 1000000 }
   validates :user_id, presence: true
 
